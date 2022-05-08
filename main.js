361a2203d32f470493ead6261cae6a3977d7c3fd
Edward--TacoFed-- <dwrdmendoza@yahoo.com>
2022-05-07 17:55:55 -0700

    first commit

--- main.js
+++ main.js
@@ -1,40 +1,44 @@
-// return which player won! In case of a draw return Draw!
+//codewars session.
 
-const rps = (p1, p2) => {
-  if (p1 == p2)
-    return 'Draw!';
-  
-  if (p1 == 'rock' && p2 == 'scissors')
-    return 'Player 1 won!'
-  else if (p1 == 'scissors' && p2 == 'paper')
-    return 'Player 1 won!'
-  else if (p1 == 'paper' && p2 == 'rock')
-    return 'Player 1 won!'
-  else
-    return 'Player 2 won!';
-};
+/* You ask someone their age and they always answer with "x years old",
+where x is a random number between 0 and 9.
 
-//Write function bmi that calculates body mass index (bmi = weight / height2).
+write a program that returns the age(0-9) as an integer.a
 
-function bmi(weight, height) {
-  const bmi = weight / (height * height);
-  if(bmi <= 18.5)
-    return "Underweight";
-  if(bmi <= 25.0)
-    return "Normal"
-  if(bmi <= 30.0)
-    return "Overwight"
-  if(bmi > 30)
-  return "Obese";
+assume the test input string is always a valid string. For example, the 
+test input may be "1 year old" or "5 years old". The first character in the string is always a number. */
+
+function getAge(inputString){
+  //parsesInt() parses a string and returns a whole number. Spaces are allowed. Only the first number is returned.
+  return parseInt(inputString);
 }
+---------------------------------------------------------------------------
 
-//multiply a given number by eight if it is an even number and by nine other wise.
+/* who rememnbers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following pharses each time a petal was torn:
 
-function simpleMultiplication(number) {
-  if(number % 2 == 0){
-    return number * 8
-  }else{
-    return number * 9
+* I love you 
+* alittle
+* alot
+* passionately
+* madly
+* not at all 
+
+when the last petal was torn there were cries of excitement, dreams, surghing thoughts and emotions.I
+your goal in this katat is to determine which pahrse the girls would say for a flower of a given number of petals, nb_petals > 0. */
+
+function howmUchILoveYou(nbPetals){
+  let x = nbPetals % 6
+  if(x === 1){
+    return 'I love you'
+  }else if(x===2){
+    return 'alittle'
+  }else if(x===3){
+    return 'alot'
+  }else if(x===4){
+    return 'passionately'
+  }else if(x===5){
+    return 'madly'
+  }else if(x===6 || x===0){
+    return 'not at all'
   }
-  }
-  
+}
